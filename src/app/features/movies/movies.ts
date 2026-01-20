46b5635b75862059a52b199f17f23c42e5e279c1
jjreina <jreinapascual@gmail.com>
2026-01-20 11:52:12 +0100

    minor refactor

--- src/app/features/movies/movies.ts
+++ src/app/features/movies/movies.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { Movie } from '../models/model';
-import { moviesMock } from '../mocks/movies-mock';
+import { Movie } from './models/model';
+import { moviesMock } from './mocks/movies-mock';
 
 @Component({
   selector: 'app-movies',
